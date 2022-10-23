576d3b0d144a74881fde23e246740be7518a727c
natebush707 <natebush707@gmail.com>
2022-10-23 12:33:16 -0700

    Add pc_plus_4 and tb_pc_plus_4

---
+++ project_4.srcs/sim_1/new/tb_pc_plus_4.sv
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Class: CSE 4010 Fall 2022
+// Engineers: Nathan Bush, Axel Lira, Steve Flores
+// 
+// Create Date: 10/23/2022 11:29:11 AM
+// Design Name: Program Counter +4 Test Bench
+// Module Name: tb_pc_plus_4
+// Project Name: Single Cycle Processor
+// Description: Test bench. UAT = pc_plus_4, a module adding 4 to the current PC.
+// 
+// Dependencies: pc_plus_4 module
+// 
+// Revision:
+// Revision 0.01 - File Created
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_pc_plus_4();
+    logic [31:0]current, next;
+    
+    pc_plus_4 tb_PC_plus4(.next(next), .current(current));
+    
+    initial
+        begin
+            current <= 'h00000000; #20;
+            assert (next == 'h00000004) else $error("next should be x0000004");
+            
+            current <= 'h00000004; #20;
+            assert(next == 'h00000008) else $error("next should be x00000008");
+            
+            current <= 'h00000008; #20;
+            assert(next == 'h0000000c) else $error("next should be x0000000c");
+        end
+
+endmodule
